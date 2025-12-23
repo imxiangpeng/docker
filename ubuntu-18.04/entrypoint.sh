3cd03daafafb8d0d2400a016515ad5862fed3833
Alex Meng <mengxp@inspur.com>
2025-12-23 09:34:14 +0800

    update
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- ubuntu-18.04/entrypoint.sh
+++ ubuntu-18.04/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-/usr/sbin/sshd
+# install packages, env or file
+if id -Gn | grep &>/dev/null '\bsudo\b'; then
+  if [ -n "$PACKAGES_LIST" ]; then
+    sudo apt-get update
+    sudo apt-get install -qy --no-install-recommends $PACKAGES_LIST
+  fi
+  if [ -f /packages.list ]; then
+    sudo apt-get update
+    sudo apt-get install -qy --no-install-recommends $(cat /packages.list)
+  fi
 
-/bin/bash
+# drop sudo privilege
+sudo /bin/rm -f /etc/sudoers.d/priv_sudo
+fi
 
+exec "$@"
