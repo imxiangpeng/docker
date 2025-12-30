80f69919dad19c19aa734f1d9fbe44079b44ea1e
Alex Meng <mengxp@inspur.com>
2025-12-30 17:37:26 +0800

    auto create user using env
    pass env UID/GID/USER
    such as:
    docker run -t --user root -e UID=111 -e GID=113 -e USER=jenkins
    
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- ubuntu-18.04/entrypoint.sh
+++ ubuntu-18.04/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+USER_UID=${UID:-$(id -u)}
+USER_GID=${GID:-$(id -g)}
+USER_NAME=${USER:-"user"}
+
 # install packages, env or file
-if id -Gn | grep &>/dev/null '\bsudo\b'; then
+if id -Gn | grep &>/dev/null '\bsudo\b' || [ "$(id -u)" -eq 0 ]; then
   if [ -n "$PACKAGES_LIST" ]; then
     sudo apt-get update
     sudo apt-get install -qy --no-install-recommends $PACKAGES_LIST
@@ -11,8 +15,21 @@ if id -Gn | grep &>/dev/null '\bsudo\b'; then
     sudo apt-get install -qy --no-install-recommends $(cat /packages.list)
   fi
 
-# drop sudo privilege
-sudo /bin/rm -f /etc/sudoers.d/priv_sudo
+  # drop sudo privilege
+  sudo /bin/rm -f /etc/sudoers.d/priv_sudo
+
+  # when running with root privilege
+  # allow auto create missing group and user with gid & uid
+  if [ "$(id -u)" -eq 0 ]; then
+    if ! getent group $USER_GID &>/dev/null; then
+      groupadd -g "$USER_GID" "group_$USER_GID"
+    fi
+
+    if ! getent passwd $USER_UID &>/dev/null; then
+      useradd -u "$USER_UID" -g "$USER_GID" -m "$USER_NAME"
+      echo $USER_NAME:123 | chpasswd
+    fi
+  fi
 fi
 
 exec "$@"
